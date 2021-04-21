5000a8d92acdf66bea128c65c5f7746f9f826df4
Thethinker20 <1170237@alumno.um.edu.mx>
2021-04-21 12:33:12 -0500

    update

--- src/public/js/regist.js
+++ src/public/js/regist.js
@@ -10,15 +10,15 @@ $(window).on("load", function () {
 });
 
   /* To Disable Inspect Element */
-  // $(document).bind("contextmenu",function(e) {
-  //   e.preventDefault();
-  //  });
+  $(document).bind("contextmenu",function(e) {
+    e.preventDefault();
+   });
    
-  //  $(document).keydown(function(e){
-  //      if(e.which === 123){
-  //         return false;
-  //      }
-  //  });
+   $(document).keydown(function(e){
+       if(e.which === 123){
+          return false;
+       }
+   });
 
 $("#txtNewPassword").change(function () {
   var password = $("#txtNewPassword").val();
